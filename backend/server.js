6da43010e64caf7660046e7f8846b9db5df4d194
Bruno Shema <shemab71@gmail.com>
2025-08-15 10:11:10 +0200

    first deploy

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+import express from 'express'
+import cors from 'cors'
+
+const PORT =  process.env.PORT || 3000;
+
+const app = express();
+
+app.use(cors());
+
+app.get('/',(req,res)=>{
+    res.json({msg: "backend am in !!!"});
+})
+
+app.listen(PORT,()=>{
+    console.log(`Bckend is listening on port: ${PORT}`);
+})
---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div> Frontend am in all ready</div>
+  )
+}
+
+export default App
