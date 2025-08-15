3f3e08828b09df71995ad277547e08c42139f87f
Bruno Shema <shemab71@gmail.com>
2025-08-15 12:50:15 +0200

    cors spiral

--- backend/server.js
+++ backend/server.js
@@ -4,20 +4,8 @@ import cors from "cors";
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-const allowedOrigins = [
-  "https://test-6jby.onrender.com", // production frontend
-  "http://localhost:5173",           // dev frontend port
-  "http://localhost:5174"            // add any other dev port you use
-];
 
-app.use(cors({
-  origin: (origin, callback) => {
-    // allow Postman or curl with no origin
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.includes(origin)) return callback(null, true);
-    callback(new Error("Not allowed by CORS"));
-  }
-}));
+app.use(cors());
 
 app.get("/api/", (req, res) => {
   res.json({ msg: "backend am in !!!" });
